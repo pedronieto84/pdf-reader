c62a8a1270accd8a8c3c27dff23d4cb9df26807e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-07-29 11:27:27 +0200

    AÑADO types de Relació de bens

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -1,18 +1,41 @@
-// Tipos TypeScript para la aplicación PDF Reader
+// Tipos TypeScript para la aplicación PDF Reader RELACIÓ DE BENS
+
 export interface PDFData {
     text: string;
 }
 
 export interface SanitizedObject {
+    id:string;
     classificacio?: number;
+
     numeroDeBe: number;
+    etiqueta?: string | null;
+    dataAlta: string; // Formato dd/mm/yyyy
+
     quantitat: number | null;
     descripcio: string;
-    centreGestor: number | null;
-    centreDeCost: number | null;
-    titolAdquisicio: string; // QUIZAS ES UN ENUM
-    costAdquisicio: string | number;
-    dataAlta: string; // Formato dd/mm/yyyy
+
+    centreGestor?: number | null;
+    centreDeCost?: number | null;
+    titular?: string | null;
+
+    tipusAdquisició?: TipusAdquisicio 
+    situacioPatrimonial?: SituacioPatrimonialType
+    naturalesaJuridica?: NaturalesaJuridica | null;
+    us?: Us;
+    vBC: string // isFormattedNumber
+    fA: string // isFormattedNumber
+    fP: string // isFormattedNumber
+    vC: string // isFormattedNumber
+
+    dotAmort?: string | null  // isFormattedNumber
+    vMercat: string 
+    vAsseguran: string 
+    vRU: string | null
+
+    dataVMercat?: string | null; // Formato dd/mm/yyyy
+    dataVAsseguran?: string | null; // Formato dd/mm/yyyy
+    dataVRU?: string | null; // Formato dd/mm/yyyy
 
 }
 
@@ -29,3 +52,26 @@ export interface DetectionResult {
     error?: string;
     [key: string]: any;
 }
+
+export type SituacioPatrimonialType= "PROPIETAT" | "CEDIT A FAVOR" | "CEDIT EN CONTRA" | "ADSCRIT A FAVOR" | "ADSCRIT EN CONTRA" | "ARRENDAT A FAVOR" | "CONCESSIÓ EN CONTRA" | "RETIRADA PERMANENT DE L'ÚS"
+
+export type NaturalesaJuridica = "PATRIMONIAL" | "DOMINI PÚBLIC - ÚS PÚBLIC" | "DOMINI PÚBLIC - SERVEI PÚBLIC" | "COMUNAL" | "PATRIMONI PÚBLIC DEL SÒL"
+
+export type Us = "ÚS COMÚ GENERAL" | "ÚS COMÚ ESPECIAL" | "ÚS PRIVATIU"
+
+export type TipusAdquisicio = "COMPRA" 
+| "CESSIÓ GRATUÏTA DE LA PROPIETAT O DONACIÓ" 
+| "CESSIÓ URBANÍSTICA OBLIGATÒRIA" 
+| "ALTA PER OBRA EN CURS" 
+| "TREBALLS FETS AMB MITJANS PROPIS"
+| "CESSIÓ D'ÚS TEMPORAL"
+| "ADSCRIPCIÓ I ALTRES APORTACIONS DE BÉNS I DRETS DE L'ENTITAT PROPIETÀRIA"
+| "INSPECCIÓ FÍSICA O INVENTARI" // iNVENTARI, INVESTIGACIÓ O INSPECCIÓ FÍSICA
+| "EXPROPIACIÓ"
+| "ADJUDICACIÓ EN PAGAMENT DE DEUTES O EMBARGAMENT"
+| "ADJUDICACIÓ EN PAGAMENT DE DEUTES O EMBARGAMENT EXERCICI TANCAT"
+| "ARRENDAMENT"
+| "ALTA PER ARRENDAMENT FINANCER (LÍSING I ALTRES)"
+| "COMPRA AMB PAGAMENT FRACCIONAT"
+| "ALTA PER LÍSING AL VENEDOR (LEASE - BACK)"
+| "ALTA PER MODIFICACIONS DE TANCAMENT"
--- src/helpers/sanitizeFunctions.ts
+++ src/helpers/sanitizeFunctions.ts
@@ -1,55 +1,86 @@
 // Funciones de sanitización para PDF Reader
-import { SanitizedObject } from './interfaces';
+import { SanitizedObject } from "./interfaces";
+
+function isFormattedNumber(value: string): boolean {
+  return (
+    value === "0" ||
+    /^(\d{1,3}(\.\d{3})*|\d+),\d{2}$/.test(value)
+  );
+}
 
 export function findClassificacio(array: string[]): string | null {
-    const regex = /\b\d{6}\b/;
-    console.log('regex', array);
-    for (let i = 0; i < array.length; i++) {
-        const match = array[i].match(regex);
-        if (match) {
-            console.log('match found:', match);
-            return match[0]; // Retorna el código de 6 dígitos encontrado
-        }
+  const regex = /\b\d{6}\b/;
+  console.log("regex", array);
+  for (let i = 0; i < array.length; i++) {
+    const match = array[i].match(regex);
+    if (match) {
+      console.log("match found:", match);
+      return match[0]; // Retorna el código de 6 dígitos encontrado
     }
-    return null; // Si no encuentra ningún código de 6 dígitos
+  }
+  return null; // Si no encuentra ningún código de 6 dígitos
 }
 
- function findNumeroDeBe(item:string): number | null {
-    const splitted = item.split(' ');
-    return splitted[0]  ? parseInt(splitted[0]) : null;
+function findNumeroDeBe(item: string): number | null {
+  const splitted = item.split(" ");
+  return splitted[0] ? parseInt(splitted[0]) : null;
 }
 
- function findQuantitat(item:string):number | null {
-    console.log('item:', item);
-    const splitted = item.split(' ');
-    console.log('splitted:', splitted);
-    return splitted[splitted.length-1] ? parseFloat(splitted[splitted.length-1]) : null;
+function findQuantitat(item: string): number | null {
+  console.log("item:", item);
+  const splitted = item.split(" ");
+  console.log("splitted:", splitted);
+  return splitted[splitted.length - 1]
+    ? parseFloat(splitted[splitted.length - 1])
+    : null;
 }
-    
 
-export function sanitizeObject(array: string[]): SanitizedObject {
-    const object: SanitizedObject = {};
-    // Classificació 120103 numeric, 6 digits
-    const classificacio = findClassificacio(array);
-    console.log('classificacio:', classificacio);
-    if (classificacio) {
-        object.classificacio = parseInt(classificacio, 10);
+function findData(array: string[]): string | null {
+  const regex = /\b\d{2}\/\d{2}\/\d{4}\b/;
+  for (let i = 0; i < array.length; i++) {
+    const match = array[i].match(regex);
+    if (match) {
+      return match[0];
     }
+  }
+  return null;
+}
 
-    // Encontrar numeroDeBe
-    const numeroDeBe = findNumeroDeBe(array[1]);
-    if(numeroDeBe){
-        object.numeroDeBe = numeroDeBe;
-    }
+export function sanitizeObject(array: string[]): SanitizedObject{
+  const object: SanitizedObject = {};
 
-    // Encontrar quantitat
-    const quantitat = findQuantitat(array[1]);
-    console.log('quantitat', quantitat)
-    
-        object.quantitat = quantitat;
-    
 
-    // Encontrar D.Alta
-    console.log('Objeto creado:', object);
-    return object; // Retorna el objeto sanitizado
+  // Classificació 120103 numeric, 6 digits
+  const classificacio = findClassificacio(array);
+  if (classificacio) {
+    object.classificacio = parseInt(classificacio);
+  }
+
+  // Encontrar numeroDeBe
+  const numeroDeBe = findNumeroDeBe(array[1]);
+  if (numeroDeBe) {
+    object.numeroDeBe = numeroDeBe;
+  }
+
+  // Encontrar quantitat
+  const quantitat = findQuantitat(array[1]);
+  if (quantitat) {
+    object.quantitat = quantitat;
+  }
+  object.quantitat = quantitat;
+
+  // Encontrar D.Alta
+  const dAlta = findData(array);
+  if (dAlta) {
+    object.dataAlta = dAlta;
+  }
+  console.log("Objeto creado:", object);
+  object.id = `${object.classificacio}-${object.dataAlta}-${object.numeroDeBe}`; // Genera un ID único basado en classificació, dataAlta y numeroDeBe
+
+
+  //
+
+
+
+  return object; // Retorna el objeto sanitizado y la classificació extraída
 }
