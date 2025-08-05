e08a5a7563bf1bc65e5c88de9f41f6e5984b99c5
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-08-05 15:21:16 +0200

    Cojo el tipado Root, que es el response del endpoint ahora mimso tal y como esta para todo un pueblo para A

--- node-server/src/helpers.ts
+++ node-server/src/helpers.ts
@@ -13,15 +13,29 @@ const tableStructure= {
     col0: 0, col1: 50.4, col2: 92.16, col3: 119.52, col4: 280.08, col5: 323.28, col6: 414, col7: 489.6, col8:598.32, col9: 696.96, col10: 747.36, filaTop: 176.4, grosorYTotales: 36.72
 }
 
+// Quitar los elementos por encima de la filaTop
+export function removeHeader(
+textBlock: TextBlock[],
+){
+
+  const filaTop = tableStructure.filaTop;
+  const filteredItems = textBlock.filter((item: TextBlock) => {
+    // Verificar si el item tiene una propiedad bbox y si su valor y0 es mayor que filaTop
+    if (item.bbox && item.bbox[1] > filaTop) {
+      return true; // Mantener el item
+    }
+    return false; // Eliminar el item
+  }); 
+
+  return [...filteredItems];
+
+}
+
 // Cuando estamos en una pagina, los items estan aqui $.data.resultado.items[0].text_blocks
-export function getPageData(data:any): TextBlock[] {
+export function getPageData(data:Root) {
 
   // bbox ex [x0, y0, x1 , y1]
 
-   
-
-    return data;
-}
 
 export function getHorizontalLines(data:any){
   const lines = data.data.horizontal_lines
--- node-server/src/interfaces.ts
+++ node-server/src/interfaces.ts
@@ -1,39 +1,84 @@
 export interface Root {
+  success: boolean
   message: string
-  parametros: Parametros
-  archivo: Archivo
-  resultado: Resultado
+  parameters: Parameters
+  file_info: FileInfo
+  data: Data
+  timestamp: string
 }
 
-export interface Parametros {
+export interface Parameters {
+  poble: string
+  informe: string
+  pag: number
+}
+
+export interface FileInfo {
+  source: string
+  file_name: string
+  file_path: string
+}
+
+export interface Data {
+  message: string
+  parameters: Parameters2
+  pdf_info: PdfInfo
+  data: Data2
+  timestamp: string
+  filter_info: FilterInfo
+}
+
+export interface Parameters2 {
   poble: string
   informe: string
   pag: any
 }
 
-export interface Archivo {
-  nombre: string
-  ruta: string
+export interface PdfInfo {
+  file_path: string
+  file_name: string
+  extraction_mode: string
 }
 
-export interface Resultado {
+export interface Data2 {
   total_pages: number
+  horizontal_lines: HorizontalLine[]
   processed_pages: number
   items: Item[]
+}
+
+export interface HorizontalLine {
+  pageNumber: number
+  horizontal_lines: HorizontalLines
+}
+
+export interface HorizontalLines {
+  number: number
+  yPositions: number[]
 }
 
 export interface Item {
   page_number: number
   text: string
-  text_blocks: TextBlock[]
+  words: Word[]
   images: any[]
   links: any[]
 }
 
-export interface TextBlock {
+export interface Word {
   text: string
   bbox: number[]
-  font: string
-  size: number
-  flags: number
+  block_number: number
+  line_number: number
+  word_number: number
+  x0: number
+  y0: number
+  x1: number
+  y1: number
+}
+
+export interface FilterInfo {
+  requested_page: number
+  page_found: boolean
+  message: string
 }
