e2994bf940adb6241f7e36ac2568e7f683b379a2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2025-08-05 12:31:59 +0200

    Cambios donde pongo en helpers.ts las medidas de la tabla.

--- node-server/src/helpers.ts
+++ node-server/src/helpers.ts
@@ -8,3 +8,11 @@ import {
 } from "./interfaces.js";
 
 // Aquí puedes agregar funciones helper que utilicen las interfaces importadas
+
+const tableStructure= {
+    col0: 0, col1: 50.4, col2: 92.16, col3: 119.52, col4: 280.08, col5: 323.28, col6: 414, col7: 489.6, col8:598.32, col9: 696.96, col10: 747.36, filaTop: 176.4, grosorYTotales: 36.72
+}
+
+export function getPageData(){
+
+}
